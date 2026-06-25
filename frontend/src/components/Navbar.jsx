c4159004f640b53f61bb6d2290c7bdf3b4cb49aa
Darshitha S R <darshitha7587@gmail.com>
2026-06-25 11:14:32 +0530

    Admin and Components Completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../styles/navbar.css";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+
+    const {user,logout}=useContext(AuthContext);
+    const navigate=useNavigate();
+    const handleLogout=()=>{
+        logout();
+        navigate("/login");
+    };
   return (
     <nav className="navbar">
         <div className="navbar-brand">
@@ -12,8 +20,17 @@ const Navbar = () => {
          </div>
          <ul className="navbar-links">
           <li><Link to="/shop">Home</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
           <li><Link to="/cart">Cart</Link></li>
+          {user ? (
+            <>
+              <li><Link to="/profile">Hi, {user.name}</Link></li>
+              {user.role === 'admin' && <li><Link to="/admin">Admin </Link></li>}
+              <li><button onClick={handleLogout} className="btn-logout">Logout</button></li>
+            </>
+          ):(
+            <li><Link to="/login">Login</Link></li>
+
+          ) }
          </ul>
       </nav>
   );
